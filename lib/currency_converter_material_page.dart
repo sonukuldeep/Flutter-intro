072f5dc32da47d03238b6762d16054d476a1b09c
sonukuldeep <sonukuldeep@outlook.com>
2023-08-15 21:38:53 +0530

    text decoration done

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -5,11 +5,49 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.white,
+        width: 2.0,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(20),
+      ),
+    );
+
     return const Scaffold(
-        body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [Text("Hello world")],
-    )));
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "0",
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                focusedBorder: border,
+                enabledBorder: border,
+                hintText: 'Please enter the amount in USD',
+                hintStyle: TextStyle(color: Colors.black),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
